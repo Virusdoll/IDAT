eaeebafc8c0ee579436a5771590f0697c835210c
Virusdoll <1029706584@qq.com>
2020-09-14 22:32:38 +0800

    new feature: multiprocessing

--- IDAT.py
+++ IDAT.py
@@ -1,10 +1,12 @@
 from PIL import Image as Image
 from torchvision import transforms as transforms
+from multiprocessing import cpu_count, Process, Pool, Manager
+from time import time, sleep
 import shutil
 import json
 import os
 import sys
-import time
+
 
 class MyCrop(object):
     '''
@@ -22,13 +24,10 @@ class MyCrop(object):
         output_img = transforms.RandomCrop(size=(crop_h, crop_w))(img)
         return output_img
 
+
 class IDAT(object):
 
     def __init__(self):
-        # 待保存数据列表(即处理完成的数据列表)
-        self.result_list = []
-        # 进行一次tf处理后的暂存数据列表
-        self.tmp_list = []
         # tf处理列表
         self.job_list = None
 
@@ -43,6 +42,9 @@ class IDAT(object):
         self.input_path = config.get('input_path')
         self.output_path = config.get('output_path')
         self.resize_limit = config.get('resize_limit')
+        self.multi_process = config.get('multi_process')
+
+        if self.multi_process == -1: self.multi_process = cpu_count()
         
         tmp_job_list = []
         for parallel_job in config.get('job'):
@@ -93,15 +95,29 @@ class IDAT(object):
             'tf': tf,
             'times': times
         }
-  
+
+    def process_image(self, process_dict, process_n):
+        '''
+        process images from file_path_list
+        '''
+        for file_path in self.file_path_list[process_n]:
+            self.process_single_image(file_path[0], file_path[1])
+            process_dict[process_n] += 1
+
     def process_single_image(self, image_path, save_path):
+        '''
+        process one image and save result
+        '''
 
         base_image = Image.open(image_path).convert('RGB')
         base_name = os.path.basename(image_path).split('.')[0]
+        result_list = []
+        tmp_list = []
 
-        max_length = base_image.size[0]
-        if base_image.size[1] > max_length:
-            max_length = base_image.size[1]
+        img_w = base_image.size[0]
+        img_h = base_image.size[1]
+        max_length = img_w if img_w > img_h else img_h
+
         if max_length > self.resize_limit:
             resize_p = self.resize_limit / max_length
             base_image = transforms.Resize((
@@ -109,66 +125,140 @@ class IDAT(object):
                 int(base_image.size[0] * resize_p)
             ))(base_image)
 
-        self.result_list.append({
-            'image': base_image,
-            'name': base_name
+        result_list.append({
+            'image'     : base_image,
+            'name'      : base_name,
+            'job_number': 0
         })
 
-        # 处理任务列表
-        for parallel_job in self.job_list:
-            # 处理并列任务
-            for job in parallel_job:
-                job_times = job.get('times')
-                # 处理结果集中的每张图片
-                for result in self.result_list:
-                    tmp_image = result.get('image')
-                    tmp_name = result.get('name')
-                    # 对每张图片进行对应次数的处理
-                    for times in range(job_times):
-                        self.tmp_list.append({
-                            'image': job.get('tf')(tmp_image),
-                            'name': '{}_{}'.format(tmp_name, job.get('func'))
-                        })
-            self.result_list.extend(self.tmp_list)
-            self.tmp_list = []
-        
         # delete base image
         os.remove(image_path)
 
-        # 保存结果
-        for result in self.result_list:
-            result.get('image').save(
-                os.path.join(
-                    save_path,
-                    '{}.jpg'.format(result.get('name'))
-                )
-            )
+        '''
+        for less memory cost,
+        use algorithm like:
+             t(1)  t(2)   t(3)    t(4)     t(5)
+        1 -> 1a -> 1ab -> 1abc -> 1abcd -> empty
+                -> 1ac -> 1acd
+                -> 1ad
+          -> 1b -> 1bc -> 1bcd
+                -> 1bd
+          -> 1c -> 1cd
+          -> 1d
+        after gen data in t(n),
+        data in t(n-1) would be saved to disk and deleted to free memory
+        when it can't gen any data(like t(5)),
+        algorithm will stop
+        '''
+        
+        job_list_len = len(self.job_list)
 
-        # 清空list
-        self.result_list = []
+        while len(result_list) > 0:
+            for result in result_list:
+                result_image        = result.get('image')
+                result_name         = result.get('name')
+                result_job_number   = result.get('job_number')
+
+                result_image.save(
+                    os.path.join(save_path, '{}.jpg'.format(result_name))
+                )
+
+                result = None
+
+                for parallel_job, job_number in zip(
+                    self.job_list,
+                    range(1, job_list_len + 1)
+                ):
+                    if job_number <= result_job_number:
+                        continue
+                    
+                    for job in parallel_job:
+                        job_tf      = job.get('tf')
+                        job_func    = job.get('func')
+                        job_times   = job.get('times')
+                        
+                        for times in range(job_times):
+                            tmp_list.append({
+                                'image'     : job_tf(result_image),
+                                'name'      : '{}_{}'.format(result_name, job_func),
+                                'job_number': job_number
+                            })
+            
+            result_list = tmp_list
+            tmp_list = []
 
     def seconds_to_time(self, seconds):
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
-        return [h, m, s]
+        return [int(h), int(m), int(s)]
+
+    def process_info(self, process_dict):
+        start_time = time()
+        finish_number = 0
+        eta_time = 0
+        cost_time = 0
+        info_format = (
+            '\r  > {:.2f}%({}/{}) ' +
+            '| cost {:0>2d}:{:0>2d}:{:0>2d} ' +
+            '| eta {:0>2d}:{:0>2d}:{:0>2d}'
+        )
+
+        while finish_number < self.file_number:
+            new_cost_time = time() - start_time
+            
+            new_finish_number = 0
+            for process_n in range(self.multi_process):
+                new_finish_number += process_dict[process_n]
+
+            if new_finish_number == finish_number:
+                eta_time = eta_time - (new_cost_time - cost_time)
+            
+            cost_time = new_cost_time
+
+            if new_finish_number > finish_number:
+                finish_number = new_finish_number
+                eta_number = self.file_number - finish_number
+                eta_time = cost_time / finish_number * eta_number
+            
+            if eta_time < 0:
+                eta_time = 0
+
+            c_h, c_m, c_s = self.seconds_to_time(cost_time)
+            e_h, e_m, e_s = self.seconds_to_time(eta_time)
+
+            print(info_format.format(
+                finish_number / self.file_number * 100,
+                finish_number, self.file_number,
+                c_h, c_m, c_s,
+                e_h, e_m, e_s
+            ), end='')
+
+            sleep(1)
 
     def main_work(self, config_path):
+        '''
+        main program of this class
+        '''
+
         # load config
-        print('load config')
+        print('  > load config')
         self.load_config(config_path)
 
         # check output path
         if os.path.exists(self.output_path):
-            raise Exception('path "{}" exists !'.format(self.output_path))
+            raise Exception('output path "{}" exists'.format(self.output_path))
 
         is_file = os.path.isfile(self.input_path)
         is_dir = os.path.isdir(self.input_path)
 
         if not (is_file or is_dir):
-            raise Exception('input path error')
+            raise Exception('input path "{}" error'.format(self.input_path))
+
+        if not self.multi_process > 0:
+            raise Exception('process number "{}" error'.format(self.multi_process))
         
         # copy all dirs and files
-        print('copy files and dirs')
+        print('  > copy files and dirs')
         
         # is file
         if is_file:
@@ -179,38 +269,52 @@ class IDAT(object):
         if is_dir:    
             shutil.copytree(self.input_path, self.output_path)
 
-        # count image number
-        total = 0
-        for root, dirs, files in os.walk(self.output_path):
-            total += len(files)
-
-        # process all files
-        print('start processing')
-        file_path_list = []
+        # 分配各线程处理的任务
+        self.file_path_list = []
+        for process_n in range(self.multi_process):
+            self.file_path_list.append([])
         
+        process_n = 0
         for root, dirs, files in os.walk(self.output_path):
             for file_name in files:
-                file_path_list.append([os.path.join(root, file_name), root])
+                self.file_path_list[process_n].append(
+                    [os.path.join(root, file_name), root]
+                )
+                process_n = 0 if process_n == self.multi_process - 1 else process_n + 1
         
-        # set count
-        count = 0
+        # count file number
+        self.file_number = 0
+        for file_path in self.file_path_list:
+            self.file_number += len(file_path)
         
-        # start time
-        start_time = time.time()
-        
-        for file_path in file_path_list:
-            self.process_single_image(file_path[0], file_path[1])
+        # create dict for process communication
+        process_dict = Manager().dict()
+        for process_n in range(self.multi_process):
+            process_dict[process_n] = 0
 
-            count += 1
-            cost_time = time.time() - start_time
-            eta_time = cost_time * (total - count) / count
-            c_h, c_m, c_s = self.seconds_to_time(cost_time)
-            e_h, e_m, e_s = self.seconds_to_time(eta_time)
-            print('\rfinish {}/{}({:.2f}%), cost {:.0f}:{:.0f}:{:.2f}, eta {:.0f}:{:.0f}:{:.2f}'.format(
-                count, total, count/total*100, c_h, c_m, c_s, e_h, e_m, e_s
-            ), end='')
-        
-        print('\nfinish')
+        # process all files
+        print('  > start processing with {} process'.format(self.multi_process))
+
+        # create and run process
+        pool = Pool(self.multi_process)
+        for process_n in range(self.multi_process):
+            pool.apply_async(
+                self.process_image,
+                args=(process_dict, process_n,)
+            )
+        pool.close()
+
+        process_info = Process(
+            target=self.process_info,
+            args=(process_dict,)
+        )
+        process_info.start()
+
+        pool.join()
+        process_info.join()
+
+        # finish
+        print('\n  > finish')
 
 if __name__ == "__main__":
     IDAT()(sys.argv[1])
